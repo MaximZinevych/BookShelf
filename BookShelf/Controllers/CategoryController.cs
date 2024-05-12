04f484c41ecf5dca610502a1f78778273f070257
maksym <maksym@ZEPHRYS>
2024-05-12 11:49:45 +0300

    MVC - Crud

---
+++ BookShelf/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿using BookShelf.Data;
+using BookShelf.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace BookShelf.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var categoryList = _dbContext.Categories.ToList();
+
+            return View(categoryList);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+		public IActionResult Create(Category category)
+		{
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display order cannot match Name");
+            }
+
+            if(ModelState.IsValid)
+            {
+				_dbContext.Categories.Add(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category created successfully";
+				return RedirectToAction("Index");
+			}
+
+            return View();
+			
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if(id == null || id == 0) 
+			{ 
+				return NotFound();
+			}
+
+			Category? category = _dbContext.Categories.Find(id);
+
+            if (category == null)
+            {
+				return NotFound();
+            }
+
+            return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Update(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
+			}
+
+			return View();
+
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? category = _dbContext.Categories.Find(id);
+
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			_dbContext.Categories.Remove(category);
+			_dbContext.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ BookShelf/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+		[Display(Name = "Category Name", Prompt = "Category Name")]
+		public string Name { get; set; }
+        [Display(Name = "Display Order", Prompt = "1")]
+        [Range(1, 100, ErrorMessage = "The field Display Order must be between 1 - 100.")]
+        public int DisplayOrder { get; set; }
+    }
+}
